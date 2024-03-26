bbc9071f1e9de288b781b43e4ce928e74444127d
dr-scooby <hinurali@hotmail.com>
2024-03-26 15:15:50 -0700

    added webapp folder for jsp, created home.jsp

--- SecureAppSpring/src/main/java/com/jah/secureapp/HomeController.java
+++ SecureAppSpring/src/main/java/com/jah/secureapp/HomeController.java
@@ -4,9 +4,19 @@
  */
 package com.jah.secureapp;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * 
  */
+@Controller
 public class HomeController {
 
+	
+	@RequestMapping("/")
+	public String home() {
+		
+		return "home.jsp";
+	}
 }
