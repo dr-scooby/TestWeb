d3b9ecc59a8e03518f5d1f7b7a21aa8f169f424c
dr-scooby <hinurali@hotmail.com>
2024-04-01 15:25:39 -0700

    added comments to AppSecurityConfig

---
+++ SecureAppSpring/src/main/java/com/jah/secureapp/AppSecurityConfig.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.jah.secureapp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+
+
+/**
+ * need to research the Spring security, latest version is different.
+ */
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig extends WebSecurityConfiguration{
+	
+	
+	
+	
+
+}
